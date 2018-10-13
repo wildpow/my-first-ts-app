d69bd868c638fd8cdcceb5bc0a2c8c60ac9cc883
wildpow <element1028@gmail.com>
2018-10-13 16:00:41 -0700

    statefull component with typeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import Hello2 from './Hello2';
 
 ReactDOM.render(
-  <App />,
+  <Hello2 name="TypeScript" enthusiasmLevel={10}/>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
